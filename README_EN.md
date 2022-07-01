c01ed69fe0194a282f2408f53fc80197de901479
ko <kojavaee@aliyun.com>
2022-07-01 14:09:40 +0800

    add linux

---
+++ README_EN.md
@@ -0,0 +1,68 @@
+Linux-0.11
+----------------------------------------------------------------------
+
+The legacy Linux kernel source version 0.11 which has 
+been tested under modern Linux and Mac OSX.
+
+##1. Build on Linux
+
+### 1.1. Linux Setup
+
+* Setup linux-0.11 dirstribution
+
+  Recommend Ubuntu, Debian, Mint
+
+* Tools
+
+  Needed gcc, qemu, bochs, gdb and etc
+
+* Release hardware image 
+
+  A linux-0.11 hardware image file: hdc-0.11.img
+
+  Please download it from
+  ```
+  http://www.oldlinux.org
+  or
+  http://mirror.lzu.edu.cn/os/oldlinux.org/
+  ```
+* Host PC
+
+  Now, this version already support Ubuntu16.04, enjoy it.
+
+### 1.2. hack linux-0.11
+
+* Build Linux-0.11
+
+  ```
+  make 
+  ```
+
+* Boot Linux-0.11 on qemu
+
+  ```
+  make start
+  ```
+
+* Debug Linux-0.11 in GDB
+
+  On target PC, running target image 
+
+  ```
+  make debug
+  ```
+
+  On debug PC, running GDB as follow:
+
+  ```
+  gdb tools/system
+  (gdb)target remote :1234
+  (gdb)b main
+  (gdb)c
+  ```
+
+* More help
+
+  ```
+  make help
+  ```
---
+++ include/asm/system.h
@@ -0,0 +1,73 @@
+//将系统态切换到用户态
+#define move_to_user_mode() \
+__asm__ (                   "movl %%esp,%%eax\n\t" \
+/*SS进栈，0x17二进制的10111*/	"pushl $0x17\n\t" \
+/*ESP进栈*/	                "pushl %%eax\n\t" \
+/*EFLAGS进栈*/	            "pushfl\n\t" \
+/*CS进栈，0x0f二进制的1111*/	"pushl $0x0f\n\t" \
+/*EIP进栈*/	                "pushl $1f\n\t" \
+/*利用IRET指令进行翻转*/	"iret\n" \
+	"1:\tmovl $0x17,%%eax\n\t" \
+	"movw %%ax,%%ds\n\t" \
+	"movw %%ax,%%es\n\t" \
+	"movw %%ax,%%fs\n\t" \
+	"movw %%ax,%%gs" \
+	:::"ax")
+
+#define sti() __asm__ ("sti"::)
+#define cli() __asm__ ("cli"::)
+#define nop() __asm__ ("nop"::)
+
+#define iret() __asm__ ("iret"::)
+
+#define _set_gate(gate_addr,type,dpl,addr) \
+__asm__ ("movw %%dx,%%ax\n\t" \
+	"movw %0,%%dx\n\t" \
+	"movl %%eax,%1\n\t" \
+	"movl %%edx,%2" \
+	: \
+	: "i" ((short) (0x8000+(dpl<<13)+(type<<8))), \
+	"o" (*((char *) (gate_addr))), \
+	"o" (*(4+(char *) (gate_addr))), \
+	"d" ((char *) (addr)),"a" (0x00080000))
+
+#define set_intr_gate(n,addr) \
+	_set_gate(&idt[n],14,0,addr)
+
+#define set_trap_gate(n,addr) \
+	_set_gate(&idt[n],15,0,addr)
+
+#define set_system_gate(n,addr) \
+	_set_gate(&idt[n],15,3,addr)
+
+#define _set_seg_desc(gate_addr,type,dpl,base,limit) {\
+	*(gate_addr) = ((base) & 0xff000000) | \
+		(((base) & 0x00ff0000)>>16) | \
+		((limit) & 0xf0000) | \
+		((dpl)<<13) | \
+		(0x00408000) | \
+		((type)<<8); \
+	*((gate_addr)+1) = (((base) & 0x0000ffff)<<16) | \
+		((limit) & 0x0ffff); }
+
+/*将104，即1101000存入描述符的第1，2字节*/
+//将tss或ldt基地址的低16位存入描述符的第3，4字节
+//循环右移16位，即高，低字互换
+//将互换完的第1字节，即地址的第3字节存入第5字节
+//将0x89或0x82存入第6字节
+#define _set_tssldt_desc(n,addr,type) \
+__asm__ ("movw $104,%1\n\t" \
+	"movw %%ax,%2\n\t" \
+	"rorl $16,%%eax\n\t" \
+	"movb %%al,%3\n\t" \
+	"movb $" type ",%4\n\t" \
+	"movb $0x00,%5\n\t" \
+	"movb %%ah,%6\n\t" \
+	"rorl $16,%%eax" \
+	::"a" (addr), "m" (*(n)), "m" (*(n+2)), "m" (*(n+4)), \
+	 "m" (*(n+5)), "m" (*(n+6)), "m" (*(n+7)) \
+	)
+//n:gdt的项值，addr:tss或ldt的地址，0x89对应tss,0x82对应ldt
+#define set_tss_desc(n,addr) _set_tssldt_desc(((char *) (n)),((int)(addr)),"0x89")
+#define set_ldt_desc(n,addr) _set_tssldt_desc(((char *) (n)),((int)(addr)),"0x82")
+
